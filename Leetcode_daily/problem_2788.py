b36e7e1438b77a45c29fbf35207ce96707d7eb44
akansha <akan7sha@gmail.com>
2024-12-26 12:50:12 +0530

    new leet code

--- Leetcode_daily/problem_2788.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def splitWordsBySeparator(self, words, separator):
-        """
-        :type words: List[str]
-        :type separator: str
-        :rtype: List[str]
-        """
-        return [s for w in words for s in w.split(separator) if s]
